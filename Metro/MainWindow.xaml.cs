6d346436b94979f9221b43dfc000d8b87875b565
gemilepus <31269075+gemilepus@users.noreply.github.com>
2020-12-04 11:25:44 +0800

    update to MahApps.Metro v2.4.1

--- Metro/MainWindow.xaml.cs
+++ Metro/MainWindow.xaml.cs
@@ -8,14 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Metro
 {
@@ -25,8 +17,6 @@ namespace Metro
     public partial class MainWindow : MetroWindow
     {
         KeyboardListener KListener = new KeyboardListener();
-
-
 
         public MainWindow()
         {
@@ -108,64 +98,5 @@ namespace Metro
             }
 
         }
-
-        private void Save_file() {
-
-            // These examples assume a "C:\Users\Public\TestFolder" folder on your machine.
-            // You can modify the path if necessary.
-
-
-            // Example #1: Write an array of strings to a file.
-            // Create a string array that consists of three lines.
-            string[] lines = { "First line", "Second line", "Third line" };
-            // WriteAllLines creates a file, writes a collection of strings to the file,
-            // and then closes the file.  You do NOT need to call Flush() or Close().
-            System.IO.File.WriteAllLines(@"C:\Users\Public\TestFolder\WriteLines.txt", lines);
-
-
-            // Example #2: Write one string to a text file.
-            string text = "A class is the most powerful data type in C#. Like a structure, " +
-                           "a class defines the data and behavior of the data type. ";
-            // WriteAllText creates a file, writes the specified string to the file,
-            // and then closes the file.    You do NOT need to call Flush() or Close().
-            System.IO.File.WriteAllText(@"C:\Users\Public\TestFolder\WriteText.txt", text);
-
-            // Example #3: Write only some strings in an array to a file.
-            // The using statement automatically flushes AND CLOSES the stream and calls 
-            // IDisposable.Dispose on the stream object.
-            // NOTE: do not use FileStream for text files because it writes bytes, but StreamWriter
-            // encodes the output as text.
-            using (System.IO.StreamWriter file =
-                new System.IO.StreamWriter(@"C:\Users\Public\TestFolder\WriteLines2.txt"))
-            {
-                foreach (string line in lines)
-                {
-                    // If the line doesn't contain the word 'Second', write the line to the file.
-                    if (!line.Contains("Second"))
-                    {
-                        file.WriteLine(line);
-                    }
-                }
-            }
-
-            // Example #4: Append new text to an existing file.
-            // The using statement automatically flushes AND CLOSES the stream and calls 
-            // IDisposable.Dispose on the stream object.
-            using (System.IO.StreamWriter file =
-                new System.IO.StreamWriter(@"C:\Users\Public\TestFolder\WriteLines2.txt", true))
-            {
-                file.WriteLine("Fourth line");
-            }
-        }
-
-        private void Btn_close_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
- 
-
- 
-        // **********************************************************************************
     }
 }
